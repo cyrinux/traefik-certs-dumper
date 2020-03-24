56d8fbb3451f1740535e5ace44138483379e9322
Cyril Levis <cyril@levis.name>
2020-03-24 10:52:07 +0100

    Handle several domains, need to use --domains

--- run.sh
+++ run.sh
@@ -13,21 +13,25 @@ dump() {
   log "Dumping certificates"
   traefik-certs-dumper file --version v2 --crt-name "cert" --crt-ext ".pem" --key-name "key" --key-ext ".pem" --domain-subdir --dest /tmp/work --source /traefik/acme.json >/dev/null
 
+
+  for domain in "${DOMAINS[@]}"; do
   if
-    [[ -f /tmp/work/${DOMAIN}/cert.pem && -f /tmp/work/${DOMAIN}/key.pem && -f /output/cert.pem && -f /output/key.pem ]] && \
-    diff -q ${WORKDIR}/${DOMAIN}/cert.pem /output/cert.pem >/dev/null && \
-    diff -q ${WORKDIR}/${DOMAIN}/key.pem /output/key.pem >/dev/null
+    [[ -f /tmp/work/${domain}/cert.pem && -f /tmp/work/${domain}/key.pem && -f /output/cert.pem && -f /output/key.pem ]] && \
+    diff -q ${WORKDIR}/${domain}/cert.pem /output/cert.pem >/dev/null && \
+    diff -q ${WORKDIR}/${domain}/key.pem /output/key.pem >/dev/null
   then
     log "Certificate and key still up to date, doing nothing"
   else
     log "Certificate or key differ, updating"
-    mv ${WORKDIR}/${DOMAIN}/*.pem /output/
+    mv ${WORKDIR}/${domain}/*.pem /output/
 
     if [ ! -z "${CONTAINERS#}" ]; then
       log "Trying to restart containers"
       restart_containers
     fi
   fi
+  done
+
 }
 
 restart_containers() {
@@ -86,7 +90,8 @@ _arg_restart_containers=
 
 print_help() {
   printf '%s\n' "traefik-certs-dumper bash script by Humenius <contact@humenius.me>"
-  printf 'Usage: %s [-r|--restart-containers <arg>] [-h|--help]\n' "$0"
+  printf 'Usage: %s -d|--domains <arg> [-r|--restart-containers <arg>] [-h|--help]\n' "$0"
+  printf '\t%s\n' "-d, --domains: Handle root domains passed as comma-separated container names (no default)"
   printf '\t%s\n' "-r, --restart-containers: Restart containers passed as comma-separated container names (no default)"
   printf '\t%s\n' "-h, --help: Prints help"
 }
@@ -95,10 +100,21 @@ parse_commandline() {
   while test $# -gt 0; do
     _key="$1"
     case "$_key" in
+    -d | --domains)
+      test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
+      _arg_domains="$2"
+      shift
+      ;;
     -r | --restart-containers)
       test $# -lt 2 && die "Missing value for the optional argument '$_key'." 1
       _arg_restart_containers="$2"
       shift
+      ;;
+    --domains=*)
+      _arg_domains="${_key##--domains=}"
+      ;;
+    -d*)
+      _arg_domains="${_key##-d}"
       ;;
     --restart-containers=*)
       _arg_restart_containers="${_key##--restart-containers=}"
@@ -122,20 +138,32 @@ parse_commandline() {
   done
 }
 
-split_list() {
+split_list_restart_containers() {
   IFS=',' read -ra CONTAINERS <<<"$1"
   log "Values split! Got '${CONTAINERS[@]}'"
+}
+
+split_list_domains() {
+  IFS=',' read -ra DOMAINS <<<"$1"
+  log "Values split! Got '${DOMAINS[@]}'"
 }
 
 ###############################################
 
 parse_commandline "$@"
 
+if [ -z "${_arg_domains}" ]; then
+  log "--domains is empty. Need at least one."
+else
+  log "Got value of --domains: ${_arg_domains}. Splitting values."
+  split_list_domains "${_arg_domains}"
+fi
+
 if [ -z "${_arg_restart_containers}" ]; then
   log "--restart-containers is empty. Won't restart containers."
 else
   log "Got value of --restart-containers: ${_arg_restart_containers}. Splitting values."
-  split_list "${_arg_restart_containers}"
+  split_list_restart_containers "${_arg_restart_containers}"
 fi
 
 mkdir -p ${WORKDIR}
